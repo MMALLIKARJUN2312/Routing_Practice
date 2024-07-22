a534686e3a59c27cda7ee04b00fd470119bb59c4
MMALLIKARJUN2312 <mmallikarjun4368@gmail.com>
2024-07-22 17:51:39 +0000

    routing_practice

--- src/components/NotFound/index.js
+++ src/components/NotFound/index.js
@@ -2,17 +2,15 @@
 
 import './index.css'
 
-const NotFound = () => {
-  return (
-    <div className="notfound-container">
-      <img
-        src="https://assets.ccbp.in/frontend/react-js/not-found-blog-bg.png"
-        alt="not found"
-        className="notfound-image"
-      />
-      <h1 className="notfound-heading">Not Found</h1>
-    </div>
-  )
-}
+const NotFound = () => (
+  <div className="not-found-container">
+    <img
+      src="https://assets.ccbp.in/frontend/react-js/not-found-blog-bg.png"
+      alt="not found"
+      className="not-found-image"
+    />
+    <h1 className="not-found-heading">Not Found</h1>
+  </div>
+)
 
 export default NotFound
